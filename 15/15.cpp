75dacc42250ff2803e6724de8bc992cc39f7f51e
Seohyun Lee <04seohyun@gmail.com>
2019-08-11 16:34:11 +0900

    Prob 15 Solved!

--- 15/15.cpp
+++ 15/15.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int n, m;
-vector<pair<int, int>> v;
-vector<int> g, s, w, l;
+vector< pair<int, int> > v;
+vector<int> g, s, w, l, wg;
+vector<vector<int> > li;
 
 int gr(int p)
 {
@@ -25,16 +26,36 @@ void un(int a, int b)
 int main()
 {
     scanf("%d %d", &n, &m);
-    g.resize(n);
-    s.resize(n);
-    v.resize(n);
-    w.resize(n);
-    l.resize(n);
+    g.resize(n+1);
+    s.resize(n+1);
+    v.resize(m+1);
+    w.resize(n+1);
+    l.resize(n+1);
+    li.resize(n+1);
     for(int i=0;i<m;++i) scanf("%d %d", &v[i].first, &v[i].second);
-    for(int i=0;i<n;++i) g[i]=i;
+    for(int i=0;i<=n;++i) g[i]=i;
     for(int i=0;i<m;++i) {
-        ++w[v[i].first];
-        ++l[v[i].second];
-        un(v[i].first, v[i].second);
+        ++w[v[i].first-1];
+        ++l[v[i].second-1];
+        un(v[i].first-1, v[i].second-1);
     }
+    for(int i=0;i<=n;++i) {
+        li[gr(i)].push_back(i);
+    }
+    for(int i=0;i<=n;++i) {
+        if(!li[i].size()) continue;
+        int pl=li[i][0];
+        for(auto j:li[i]) {
+            if(w[j]>w[pl]) pl=j;
+            else if(w[j]==w[pl] && l[j]<l[pl]) pl=j;
+        }
+        if(wg.empty() || s[gr(wg[0])]==s[gr(pl)]) wg.push_back(pl);
+        else if(s[gr(wg[0])]<s[gr(pl)]) {
+            wg.clear();
+            wg.push_back(pl);
+        }
+    }
+    sort(wg.begin(), wg.end());
+    printf("%d\n", wg[0]+1);
+    printf("\n");
 }
